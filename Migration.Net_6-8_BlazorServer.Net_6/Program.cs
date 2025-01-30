07dcd05238a2320d62b0b20ccd360ea89123306d
Victor Looren de Jong <v.loorendejong@centerone.nl>
2025-01-30 17:46:20 +0100

    Switched over to .Net 8 structure | Javascript works | Css not yet working properly

--- Migration.Net_6-8_BlazorServer.Net_6/Program.cs
+++ Migration.Net_6-8_BlazorServer.Net_6/Program.cs
@@ -1,14 +1,14 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Migration.Net_6_8_BlazorServer.Net_6;
 using Migration.Net_6_8_BlazorServer.Net_6.Data;
+using Migration.Net_6_8_BlazorServer.Net_6.Pages;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
-
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -23,9 +23,8 @@ app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 
-app.UseRouting();
-
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
+app.UseAntiforgery();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
 
 app.Run();
